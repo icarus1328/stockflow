d37394ed09c5de0b702f33351c0357679eeeef99
light <revibe001@gmail.com>
2026-04-22 19:44:13 +0530

    feat:initalised tables

---
+++ inventory-backend/app/models/__init__.py
@@ -0,0 +1,6 @@
+from app.models.users import User
+from app.models.category import Category
+from app.models.product import Product
+from app.models.supplier import Supplier 
+from app.models.inventory_transaction import InventoryTransaction
+from app.models.product_supplier import ProductSupplier
